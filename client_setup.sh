cff76f92f1897cb0a72b655b8877867338a5ffe4
Marina Moore <mnm678@gmail.com>
2023-08-23 15:45:09 -0400

    bug fixes
    Signed-off-by: Marina Moore <mnm678@gmail.com>

--- client_setup.sh
+++ client_setup.sh
@@ -4,13 +4,18 @@ mkdir local-repository/updater/metadata
 mkdir local-repository/updater/metadata/current
 mkdir local-repository/updater/metadata/previous
 cp registry/metadata/root.json local-repository/updater/metadata/current/root.json
+
 mkdir local-repository/registry
 mkdir local-repository/registry/metadata
 mkdir local-repository/registry/metadata/current
 mkdir local-repository/registry/metadata/previous
 cp registry/metadata/root.json local-repository/registry/metadata/current/root.json
+
 mkdir local-repository/test_repository1
 mkdir local-repository/test_repository1/metadata
 mkdir local-repository/test_repository1/metadata/current
 mkdir local-repository/test_repository1/metadata/previous
 cp test_repository1/metadata/root.json local-repository/test_repository1/metadata/current/root.json
+
+mkdir non_art_downloaded_targets
+mkdir downloaded_targets
--- without-artemis-download.py
+++ without-artemis-download.py
@@ -10,6 +10,7 @@ repository_mirrors = {'mirror1': {'url_prefix': 'http://localhost:8001',
                                     'confined_target_dirs': ['']}}
 
 updater = tuf.client.updater.Updater('updater', repository_mirrors)
+updater.refresh()
 
 target = updater.get_one_valid_targetinfo(image_name)
 updater.download_target(target, "non_art_downloaded_targets")
